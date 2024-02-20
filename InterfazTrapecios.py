16ab97bcf5f69e307c11c40492646d478868f52e
zCarreado <sergio.perez06@uptc.edu.co>
2024-02-20 11:58:53 -0500

    agregada la funcion de graficar

--- InterfazTrapecios.py
+++ InterfazTrapecios.py
@@ -1,11 +1,13 @@
 import tkinter as tk
+import matplotlib.pyplot as plt
+import numpy as np
 import Trapecios as T
 
 raiz = tk.Tk()
 raiz.resizable(False, False)
 raiz.geometry("650x350")
 
-label_raiz = tk.Label(raiz, text="Aquí aparecerá el valor de la raiz")
+label_raiz = tk.Label(raiz, text="Aquí aparecerá el valor del area")
 label_raiz.pack()
 
 def mostrar_raiz():
@@ -15,11 +17,9 @@ def mostrar_raiz():
         valor_b = int(entry_o.get())
         valor_n = int(entry_i.get())
         
-        label_raiz.config(text="El valor de la raiz es: "+ str(T.trapezoidal(valor_f, valor_a, valor_b, valor_n)))
-        return T.trapezoidal(valor_f, valor_a, valor_b, valor_n)
+        label_raiz.config(text="El valor del area es: "+ str(T.trapezoidal(valor_f, valor_a, valor_b, valor_n)))
     except ValueError:
         label_raiz.config(text="Error: Formato de datos incorrectos")
-        return "Error: Ingresa números enteros válidos" + str(valor_f)
 
 # Campo de entrada para función
 label_f = tk.Label(raiz, text="Función")
@@ -46,16 +46,30 @@ entry_i = tk.Entry(raiz)
 entry_i.pack()
 
 # Botón para mostrar mensaje
-btn_mostrar = tk.Button(raiz, text="Raíz", command=mostrar_raiz)
+btn_mostrar = tk.Button(raiz, text="Area", command=mostrar_raiz)
 btn_mostrar.pack()
 
 def graficar():
-    valor_i = int(entry_i.get())
-    valor_s = int(entry_s.get())
-    valor_o = int(entry_o.get())
-    pass
+    try:
+        valor_f = entry_f.get()
+        valor_a = float(entry_s.get())
+        valor_b = float(entry_o.get())
+        valor_n = int(entry_i.get())
 
+        # Calcular puntos para graficar
+        x_values = np.linspace(valor_a, valor_b, 100)
+        y_values = [eval(valor_f, {'x': x}) for x in x_values]
 
+        # Graficar
+        plt.figure()
+        plt.plot(x_values, y_values)
+        plt.xlabel('x')
+        plt.ylabel('f(x)')
+        plt.title('Gráfica de la función')
+        plt.grid(True)
+        plt.show()
+    except ValueError:
+        label_raiz.config(text="Error: Formato de datos incorrectos")
 
 btn_graficar = tk.Button(raiz, text="Graficar", command=graficar)
 btn_graficar.pack()
