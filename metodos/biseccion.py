703ada8c68429fa6fd6c15b8403234863587182a
FmCalistenico <leibnitz.rojas@uptc.edu.co>
2024-03-11 21:24:07 -0500

    Se reviso el metodo jacobi estableciendo los errores que se producen si se alcanza la convergencia

--- metodos/biseccion.py
+++ metodos/biseccion.py
@@ -15,7 +15,7 @@ def bisection_solver(equation_str, a, b, tol=1e-4, max_iterations=100):
     
 
     iteration = 0
-    
+    # Itera hasta que se alcance la tolerancia o el número máximo de iteraciones
     while (b - a) / 2 > tol and iteration < max_iterations:
         midpoint = (a + b) / 2
         if f(midpoint) == 0:
@@ -26,15 +26,17 @@ def bisection_solver(equation_str, a, b, tol=1e-4, max_iterations=100):
             a = midpoint
         iteration += 1
     
-
+ # Retorna la aproximación de la raíz encontrada
     return (a + b) / 2
 
 if __name__ == "__main__":
+     # Solicita al usuario ingresar la ecuación, el valor de 'a' y 'b'
     equation_str = input("Ingresa la ecuación en términos de 'x': ")
     a = float(input("Ingresa el valor de 'a': "))
     b = float(input("Ingresa el valor de 'b': "))
     
     try:
+         # Intenta encontrar la raíz y muestra el resultado
         result = bisection_solver(equation_str, a, b)
         print(f"La raíz aproximada es: {result:.4f}")
     except ValueError as e:
--- metodos/jacobi.py
+++ metodos/jacobi.py
@@ -1,20 +1,29 @@
 import numpy as np
 
 def jacobi(A, b, x0, tol=1e-6, max_iter=100):
-    
+     # Convertimos todas las entradas a matrices NumPy
     A = np.array(A, dtype=float)
     b = np.array(b, dtype=float)
     x0 = np.array(x0, dtype=float)
+    # Obtenemos la longitud del vector de términos independientes
     n = len(b)
+    # Creamos una copia del vector inicial de valores x0
     x = x0.copy()
 
+# Iteramos hasta alcanzar el número máximo de iteraciones
     for k in range(max_iter):
+        # Inicializamos un nuevo vector para almacenar los valores calculados en esta iteración
         x_new = np.zeros(n)
 
+# Iteramos sobre cada elemento del vector x_new
         for i in range(n):
+            # Calculamos el valor de x[i] utilizando la fórmula de Jacobi
             x_new[i] = (b[i] - np.dot(A[i, :i], x[:i]) - np.dot(A[i, i+1:], x[i+1:])) / A[i, i]
 
         if np.linalg.norm(x_new - x) < tol:
+
+# Comprobamos si la diferencia entre el vector de la iteración actual y la anterior es menor que la tolerancia
+
             return x_new
 
         x = x_new
@@ -31,4 +40,7 @@ if __name__ == "__main__":
         solucion_aproximada = jacobi(A, b, x0)
         print("Solución aproximada:", solucion_aproximada)
     except ValueError as e:
+
+        # Si se produce una excepción (por ejemplo, si no se alcanza la convergencia), mostramos un mensaje de error
+        
         print(e)
