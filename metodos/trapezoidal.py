0fff6bea3bb6df335bec1bba648dd35cb774bc44
FmCalistenico <leibnitz.rojas@uptc.edu.co>
2024-03-11 21:30:52 -0500

    Se reviso/ Depuro/ Corrigio la logica del metodo del Trapecio

--- metodos/trapezoidal.py
+++ metodos/trapezoidal.py
@@ -4,10 +4,18 @@ def trapezoidal(func, a, b, n):
  
     x = sp.symbols('x')
     func_expr = sp.sympify(func)
+
+     # Calculamos el ancho de cada subintervalo
     h = (b - a) / n
+
+     # Calculamos los valores de 'x' en cada punto del intervalo
     x_values = [a + i * h for i in range(n + 1)]
 
+
+# Inicializamos la integral con los términos de los extremos del intervalo
     integral = (h / 2) * (func_expr.subs(x, x_values[0]) + func_expr.subs(x, x_values[n]))
+    
+      # Sumamos los términos de la función evaluados en los puntos intermedios
     for i in range(1, n):
         integral += h * func_expr.subs(x, x_values[i])
 
@@ -15,12 +23,13 @@ def trapezoidal(func, a, b, n):
 
 if __name__ == "__main__":
 
+# Solicitamos al usuario ingresar la función, límites y número de subintervalos
     ecuacion = input("Ingresa la función en términos de 'x': ")
     a = float(input("Ingresa el límite inferior 'a': "))
     b = float(input("Ingresa el límite superior 'b': "))
     n = int(input("Ingresa el número de subintervalos 'n': "))
 
-
+    # Calculamos la aproximación de la integral usando el método del trapecio
     integral_aproximada = trapezoidal(ecuacion, a, b, n)
     print(f"Aproximación de la integral definida: {integral_aproximada:.6f}")
 
