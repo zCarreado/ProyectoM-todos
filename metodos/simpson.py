f1b32f995ba631c49b2f611cbe29b41a8e49737b
FmCalistenico <leibnitz.rojas@uptc.edu.co>
2024-03-12 14:35:01 -0500

    Se reviso/ Depuro/ Corrigio la logica del metodo SIMPSON VERIFICANDO Y PROBANDO POSIBLES ERRORES Y FALLOS

--- metodos/simpson.py
+++ metodos/simpson.py
@@ -3,11 +3,21 @@ import sympy as sp
 def simpson(func, a, b, n):
 
     x = sp.symbols('x')
+
+        # Convertimos la función dada en una expresión simbólica
     func_expr = sp.sympify(func)
+
+    # Calculamos el ancho de cada subintervalo
     h = (b - a) / n
+
+        # Calculamos los valores de 'x' en cada punto del intervalo
     x_values = [a + i * h for i in range(n + 1)]
 
+ # Inicializamos la integral con los términos de los extremos del intervalo
     integral = (h / 3) * (func_expr.subs(x, x_values[0]) + func_expr.subs(x, x_values[n]))
+
+      # Sumamos los términos de la función evaluados en los puntos intermedios,
+    # utilizando el método de Simpson para ponderar los términos pares e impares
     for i in range(1, n):
         if i % 2 == 0:
             integral += (h / 3) * 2 * func_expr.subs(x, x_values[i])
@@ -18,6 +28,7 @@ def simpson(func, a, b, n):
 
 if __name__ == "__main__":
 
+ # Solicitamos al usuario ingresar la función, límites y número de subintervalos
     ecuacion = input("Ingresa la función en términos de 'x':  ")
     a = float(input("Ingresa el límite inferior 'a': "))
     b = float(input("Ingresa el límite superior 'b': "))
